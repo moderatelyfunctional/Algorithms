b79ca8c2cb2e4b6db33b08e3e0116e8c1e104766
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2017-12-09 21:18:54 -0500

    recurisve additions to proto_van_em

--- code/van_eb_trees/proto_van.py
+++ code/van_eb_trees/proto_van.py
@@ -8,32 +8,39 @@ ONE_MASK = 0x1
 class Proto_Van_Emde:
 
     def  __init__(self, universe):
-        k = math.log(universe, TWO_BASE) / 2
+        self.u = universe
+        self.elts = [0 for _ in range(universe)]
+
+        if self.u == 2:
+            return
+
+        u_bits = math.floor(math.log(universe, TWO_BASE))
+        k = math.log(u_bits, TWO_BASE)
         integer_k = math.floor(k)
         assert(k == integer_k)
 
-        self.k = integer_k
-        self.universe = universe
-        self.sqrt_universe = math.floor(math.sqrt(self.universe))
-        self.low_mask = (ONE_MASK << self.k) - 1
-        self.high_mask = self.low_mask << self.k
+        self.sqrt_u = math.floor(math.sqrt(self.u))
+        self.u_bits = math.floor(u_bits / 2)
+        self.low_mask = (ONE_MASK << self.u_bits) - 1
+        self.high_mask = self.low_mask << self.u_bits
+
+        self.summary = Proto_Van_Emde(self.sqrt_u)
+        self.cluster = Proto_Van_Emde(self.sqrt_u)
 
     def high(self, x):
-        assert(math.floor(x / math.sqrt(self.universe)) == (x & self.high_mask) >> self.k)
-        return (x & self.high_mask) >> self.k
+        # assert(math.floor(x / self.sqrt_u) == (x & self.high_mask) >> self.u_bits)
+        return (x & self.high_mask) >> self.u_bits
 
     def low(self, x):
-        assert(math.floor(x % math.sqrt(self.universe)) == x & self.low_mask)
+        # assert(math.floor(x % self.sqrt_u) == x & self.low_mask)
         return x & self.low_mask
 
     def index(self, s_index, c_index):
-        return s_index * self.sqrt_universe + c_index
+        return s_index * self.sqrt_u + c_index
 
-p_ve = Proto_Van_Emde(64)
-for i in range(64):
-    s_index = p_ve.high(i)
-    c_index = p_ve.low(i)
-    sert(i == p_ve.index(s_index, c_index))
+p_ve = Proto_Van_Emde(16)
+#for i in range(16):
+#    s_index = p_ve.high(i)
+#    c_index = p_ve.low(i)
+#    assert(i == p_ve.index(s_index, c_index))
 
-
-# 2^(2k) = 64. k = 3. sqrt(64) = 8.
