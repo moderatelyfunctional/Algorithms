83ebc6ebc764cce11c82e06fa5ddba7c35930bae
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2017-12-09 22:34:33 -0500

    refactoring min/max

--- code/van_eb_trees/proto_van.py
+++ code/van_eb_trees/proto_van.py
@@ -41,32 +41,46 @@ class Proto_Van_Emde:
     def member(self, x):
         if self.u == 2:
             return self.elts[x]
-        return member(self.cluster[self.high(x), self.low(x)])
+        return self.cluster[self.high(x)].member(self.low(x))
 
     def insert(self, x):
         if self.u == 2:
             self.elts[x] = 1
         else:
-            insert(self.summary, self.high(x))
-            insert(self.cluster[self.high(x)], self.low(x))
+            self.summary.insert(self.high(x))
+            self.cluster[self.high(x)].insert(self.low(x))
+
+    def __min_max_helper(self, find_min=True):
+        if self.u == 2:
+            first_elt = 0 if find_min else 1
+            second_elt = 1 if find_min else 0
+            if self.elts[first_elt]:
+                return first_elt
+            if self.elts[second_elt]:
+                return second_elt
+            return None
+        c_index = self.summary.__min_max_helper(find_min)
+        if c_index == None:
+            return None
+        offset = self.cluster[c_index].__min_max_helper(find_min)
+        return self.index(c_index, offset)
 
     def minimum(self):
-        if self.u == 2:
-            if self.elts[0]:
-                return 0
-            if self.elts[1]:
-                return 1
-            return None
+        return self.__min_max_helper(True)
 
-        min_cluster = minimum(self.summary)
-        if min_cluster == None:
-            return None
-        offset = minimum(self.cluster[min_cluster])
-        return index(min_cluster, offset)
+    def maximum(self):
+        return self.__min_max_helper(False)
 
 p_ve = Proto_Van_Emde(16)
+
 #for i in range(16):
 #    s_index = p_ve.high(i)
 #    c_index = p_ve.low(i)
 #    assert(i == p_ve.index(s_index, c_index))
+
+p_ve.insert(10)
+p_ve.insert(15)
+print(p_ve.member(8))
+print(p_ve.minimum())
+print(p_ve.maximum())
 
