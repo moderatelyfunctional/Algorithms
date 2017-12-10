696d9611a5de4e0fa9d42b3ecc34314c60820d8c
Jing Lin <moderatelyfunctionalprogrammer@gmail.com>
2017-12-09 21:48:53 -0500

    insert, minimum, member

--- code/van_eb_trees/proto_van.py
+++ code/van_eb_trees/proto_van.py
@@ -25,7 +25,7 @@ class Proto_Van_Emde:
         self.high_mask = self.low_mask << self.u_bits
 
         self.summary = Proto_Van_Emde(self.sqrt_u)
-        self.cluster = Proto_Van_Emde(self.sqrt_u)
+        self.cluster = [Proto_Van_Emde(self.sqrt_u) for _ in range(self.sqrt_u)]
 
     def high(self, x):
         # assert(math.floor(x / self.sqrt_u) == (x & self.high_mask) >> self.u_bits)
@@ -37,6 +37,32 @@ class Proto_Van_Emde:
 
     def index(self, s_index, c_index):
         return s_index * self.sqrt_u + c_index
+
+    def member(self, x):
+        if self.u == 2:
+            return self.elts[x]
+        return member(self.cluster[self.high(x), self.low(x)])
+
+    def insert(self, x):
+        if self.u == 2:
+            self.elts[x] = 1
+        else:
+            insert(self.summary, self.high(x))
+            insert(self.cluster[self.high(x)], self.low(x))
+
+    def minimum(self):
+        if self.u == 2:
+            if self.elts[0]:
+                return 0
+            if self.elts[1]:
+                return 1
+            return None
+
+        min_cluster = minimum(self.summary)
+        if min_cluster == None:
+            return None
+        offset = minimum(self.cluster[min_cluster])
+        return index(min_cluster, offset)
 
 p_ve = Proto_Van_Emde(16)
 #for i in range(16):
